b7550e549c8992bc4d9945ac70af75e6b9a52d0a
Firkhraag <siglandrei@mail.ru>
2017-02-11 00:26:54 +0300

    Add test interface

---
+++ src/main/java/easytests/entities/test/CommonFieldsInterface.java
@@ -0,0 +1,11 @@
+package easytests.entities.test;
+
+import easytests.entities.User;
+
+public interface CommonFieldsInterface {
+
+    String getTestName();
+    long getId();
+    User getUser();
+
+}
