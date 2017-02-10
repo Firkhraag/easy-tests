2a87ef7f42a9b947e4ebe2db9f0c2abc0df9a436
Firkhraag <siglandrei@mail.ru>
2017-02-11 00:26:03 +0300

    Edit user interface

--- src/main/java/easytests/entities/user/CommonFieldsInterface.java
+++ src/main/java/easytests/entities/user/CommonFieldsInterface.java
@@ -1,9 +1,14 @@
 package easytests.entities.user;
 
+import easytests.entities.Test;
+import java.util.List;
+
 public interface CommonFieldsInterface {
+
     String getFirstName();
-
     String getLastName();
-
     String getSurname();
+    long getId();
+    List<Test> getTests();
+
 }
