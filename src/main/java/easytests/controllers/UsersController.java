3d539255a36aca50a1573c549958251512d581da
Firkhraag <siglandrei@mail.ru>
2017-02-11 00:23:21 +0300

    Edit user controller

--- src/main/java/easytests/controllers/UsersController.java
+++ src/main/java/easytests/controllers/UsersController.java
@@ -1,33 +1,27 @@
 package easytests.controllers;
 
 import easytests.entities.User;
-import org.apache.commons.logging.Log;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import easytests.service.UserService;
+import easytests.dao.UserDAO;
+import java.util.List;
 
-import java.io.OutputStream;
-import java.util.ArrayList;
-import java.util.logging.Logger;
-
-/**
- * Created by vkpankov on 31.10.2016.
- * Edited by SingularityA on 03.11.2016
- * Edited by malinink on 05.11.2016
- */
 @Controller
 public class UsersController {
-    @RequestMapping("/")
+
+    @RequestMapping("/users")
     @ResponseBody
-    public String list() throws Exception {
-        UserService userService = new UserService();
-        if(userService.getAllUsers().size()<3) {
-            userService.addUser(new User("Test1", "Test1", "Test1"));
-            userService.addUser(new User("Test2", "Test2", "Test2"));
-            userService.addUser(new User("Test3", "Test3", "Test3"));
-        }
-        return "Users were added successfully!";
+    public static void main(String[] args) throws Exception {
+        UserDAO userService = new UserDAO();
+        //userService.addUser(new User("Test1", "Test1", "Test1"));
+        //System.out.println("---Data inserted---");
+        User user = userService.getUserById(2);
+        System.out.println("Tests of requested user:");
+        if( user.getTests().size() == 0)
+            System.out.println("None");
+        else
+            user.getTests().forEach(test->System.out.println("id:"+test.getId()+", testName:"+test.getTestName()));
     }
+
 }
