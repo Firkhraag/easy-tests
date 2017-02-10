88e4fd623d59b8ba2b41e464e628809daf65f05e
Firkhraag <siglandrei@mail.ru>
2017-02-11 00:25:15 +0300

    Edit user entity

--- src/main/java/easytests/entities/User.java
+++ src/main/java/easytests/entities/User.java
@@ -1,13 +1,27 @@
 package easytests.entities;
-
 import easytests.entities.user.CommonFieldsInterface;
+import java.util.ArrayList;
+import java.util.List;
 
+//User POJO Class
 public class User implements CommonFieldsInterface {
+
+    private long id;
     private String firstName;
     private String lastName;
     private String surname;
+    private List<Test> tests = new ArrayList<Test>();
+
+    protected User(){}
 
     public User(String firstName, String lastName, String surname) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.surname = surname;
+    }
+
+    public User(long id, String firstName, String lastName, String surname) {
+        this.id = id;
         this.firstName = firstName;
         this.lastName = lastName;
         this.surname = surname;
@@ -27,5 +41,13 @@ public class User implements CommonFieldsInterface {
     public String getSurname() {
         return this.surname;
     }
+
+    @Override
+    public long getId() {
+        return this.id;
+    }
+
+    @Override
+    public List<Test> getTests() { return tests; }
 
 }
